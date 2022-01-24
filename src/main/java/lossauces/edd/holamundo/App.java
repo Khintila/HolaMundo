5a67f94e4e7ce864ea8927282f6403462bcc2faf
Leovigildo <you@example.com>
2022-01-24 22:31:03 +0100

    Subiendo el HolaMundo en Java

---
+++ src/main/java/lossauces/edd/holamundo/App.java
@@ -0,0 +1,12 @@
+package lossauces.edd.holamundo;
+
+/**
+ * @author Khintila
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hola mundo");
+    }
+}
